2dba1f532d9813aeb9eb0b62143bdbd8fe64299c
ChromaCyan <jofe.bugarin.up@phinmaed.com>
2025-02-07 00:12:26 +0800

    Minor fixes

--- model/moodModel.js
+++ model/moodModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const moodSchema = new Schema({
-  moodScale: { type: Number, required: true, min: 1, max: 5 },
+  moodScale: { type: Number, required: true, min: 1, max: 4 },
   moodDescription: { type: String, required: true, minlength: 2 },
   patient: { type: mongoose.Schema.Types.ObjectId, ref: 'Patient', required: true },
   createdAt: { type: Date, default: Date.now },
